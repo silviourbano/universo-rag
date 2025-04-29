f56da22baa6438cc3e0f1187d9d756cdcee752f9
Victor Sartori <victor.sartori@gmail.com>
2025-04-29 15:11:25 -0300

    bolo version

--- scripts/ingest.py
+++ scripts/ingest.py
@@ -11,8 +11,9 @@ EMBED_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
 
 def load_documents():
     loaders = [
-        PyPDFLoader(os.path.join(DATA_DIR, "procedimento_redes.pdf")),
-        TextLoader(os.path.join(DATA_DIR, "tcping_link.txt"))
+        # PyPDFLoader(os.path.join(DATA_DIR, "procedimento_redes.pdf")),
+        # TextLoader(os.path.join(DATA_DIR, "tcping_link.txt"))
+        TextLoader(os.path.join(DATA_DIR, "venda_de_bolos.txt"))
     ]
     documents = []
     for loader in loaders:
